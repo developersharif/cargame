0e6d7a8156cf03160d951cc2d4bc496a1179033c
sharif <developersharif@yahoo.com>
2025-10-02 17:05:56 +0600

    Updates

--- _app/immutable/chunks/C2a0uBIz.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1vft21n)==null?void 0:s.base)??"/cargame";var a;const e=((a=globalThis.__sveltekit_1vft21n)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/entry/start.CSpw6eHw.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BZtaPGCW.js";export{o as load_css,r as start};
